c64440ec0b08b131838a3e1681c2b07ff7d56f78
esteban1234 <tva_libra@hotmail.com>
2017-11-15 21:35:32 -0600

    pagina lista

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.jpg" />
+  <title>CEIBAS</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+    <META NAME="Title" content="CEIBAS">
+    <META NAME="Author" CONTENT="[http://zizilche.com/]"/>
+    <META name="Description" content="Lotes de Inversión"/>
+    <META NAME="Keywords" CONTENT="casas en venta, venta de casas, terrenos en venta, departamentos en venta, Morelia, Colima, Tabasco, Mérida, Querétaro, La laguna, casas CEIBAS, terrenos CEIBAS, club CEIBAS, golf"/>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto|Timmana|Contrail+One" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    
+
+
+<div class="bannerh" id="INICIO">
+  <div class="header_top"></div>
+
+  <div class="menu">
+    <figure class="logo pull-left">
+      <img src="img/ceibas.jpg" alt="CEIBAS" class="img-responsive">
+    </figure>
+
+    <nav class="pull-right">
+       <ul class="">
+          <li class=""><a class="selector" data-scroll href="#INICIO" aria-pressed="true" id="active">INDEX</a></li>
+          <li class=""><a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a></li>
+          <li class=""><a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true" >MASTER PLAN</a></li>
+          <li class=""><a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a></li>
+
+        </ul>
+    </nav>
+  </div>
+
+
+
+<div class="banner">
+  <!-- <div class="fondo"> -->
+    <!-- <p class="text-center tit">Lotes Residenciales </p>
+    <p class="subtit text-center">Adquiere tu patrimonio hoy y disfruta de los beneficios el día de mañana</p>
+    <div style="text-align: center; margin-top: 5%;">
+      <a class="btn btn-danger btn-outline btn-lg" data-scroll href="#CONTACTO">¡CONTACTANOS!</a>
+  </div> -->
+  <!-- </div> -->
+</div>
+</div>
+
+
+<section class="seccion1" id="NOSOTROS">
+  <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 style="color: #727A42;">CEIBAS ZIZILCHE</h1>
+            <h3 style="color: #7FA51A;">Haz tu vida en el norte de mérida</h3>
+            <p class="texto">Ubicada al NORTE DE MERIDA su ambiente relajado y en armonía con el entorno natural, la han convertido en una zona ideal para vivir. <br><br>
+            Está ideado y ubicado estratégicamente para adquirir una alta plusvalía en constante incremento.
+            Se localiza en el kilómetro 15 de la carretera Mérida-Progreso, en una zona de gran expectativa de exclusividad residencial. Cuenta con acceso fácil y eficiente al encontrarse a sólo 10 km de la carretera comunicándose fácilmente con las avenidas más importantes de la Mérida, cercanas a las mejores escuelas, restaurantes. <br><br>
+
+            Su ambiente relajado y en armonía con el entorno natural, la han convertido en una zona ideal para vivir. <br><br>
+            ¡Adquiere tu patrimonio hoy y disfruta de los beneficios el día de mañana! 
+            </p>
+          </div>
+          <div class="col-md-6">
+            <img src="img/ceibas.jpg"
+            class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section" style="padding: 5% 0%; background-color: #abb483 ; ">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-1 icono">
+            <i class="fa fa-usd fa-5x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-2">
+            <h4 class="text-left">FINANCIAMIENTO</h4>
+            <p class="">Ofrecemos todas las opciones de financiamiento para la adquisición de una propiedad con características competitivas.</p>
+          </div>
+          <div class="col-md-2 icono">
+            <i class="fa fa-heart fa-4x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-2">
+            <h4 class="text-left">OFERTA INMOBILIARIA</h4>
+            <p class="text-left">Nuestra oferta inmobiliaria se basa en mostrar un producto terminado, que cumpla completamente con las expectativas del clientes.</p>
+          </div>
+          <div class="col-md-2 icono">
+            <i class="fa fa-leaf fa-4x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-2">
+            <h4 class="text-left">ÁREAS VERDES</h4>
+            <p class="text-left">La naturaleza es indispensable para alcanzar una alta calidad de vida, por lo cual les ofrecemos grandes áreas verdes donde podrán disfrutar de un entorno saludable y fresco.</p>
+          </div>
+        </div> 
+        <div class="row">
+          <div class="col-md-1 icono">
+            <i class="fa fa-cogs fa-4x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-2">
+            <h4 class="text-left">SERVICIOS DE FÁCIL ACCESO</h4>
+            <p class="text-left">Pensamos en todos los servicios cotidianos que requieren sus habitantes, tomando en cuenta sus necesidades básicas de educación, salud y entretenimiento.</p>
+          </div>
+          <div class="col-md-2 icono">
+            <i class="fa fa-home fa-4x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-2 text-center">
+            <h4 class="text-left">DESARROLLOS ARMÓNICOS</h4>
+            <p class="text-left">Tenemos como visión planear desarrollos armónicos que sobrepasen los estándares de excelencia que privilegian la calidad de vida.</p>
+          </div>
+          <div class="col-md-2 icono">
+            <i class="fa fa-video-camera fa-4x" aria-hidden="true"></i>
+          </div>
+          <div class="col-md-2 text-center">
+            <h4 class="text-left">ENTORNO SEGURO</h4>
+            <p class="text-left">Incluimos altos estándares de vigilancia, seguridad completa e integración urbana, cuidando en todo momento la privacidad de nuestros clientes.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+<!-- <div class="mapa">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14683.233946160979!2d-89.70307279403909!3d21.149227604260688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f55de301e5b78a1%3A0x6538c66d1c41bea7!2sDzidzilch%C3%A9%2C+Yuc.!5e1!3m2!1ses!2smx!4v1510704759718" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div> -->
+  </section>
+
+
+<section class="seccion2" id="SERVICIOS">
+  <div class="section">
+    <div class="container">
+      <!-- <h1 class="text-center" style="color: #727A42;">Master Plan</h1> <br><br> -->
+      <!-- <div class="row">
+        
+
+
+    <div class = "col-md-3">
+        <div class="card-base">
+          <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-exclamation-circle"></span></a>
+          <div class="card-data widgetCardData">
+          <h2 class="box-title" style="color: #767A5B;">Terreno <br> excelente</h2></div>
+          </div>
+          </div>
+    </div>
+
+    <div class = "col-md-3">
+        <div class="card-base">
+          <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-usd"></span></a>
+          <div class="card-data widgetCardData">
+          <h2 class="box-title" style="color: #767A5B;">Excelente <br> precio</h2></div>
+          </div>
+          </div>
+    </div>
+
+    <div class = "col-md-3">
+        <div class="card-base">
+          <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-line-chart"></span></a>
+          <div class="card-data widgetCardData">
+          <h2 class="box-title" style="color: #767A5B;">Inversión <br> segura</h2></div>
+          </div>
+          </div>
+    </div>
+
+    <div class = "col-md-3">
+        <div class="card-base">
+          <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-map-marker"></span></a>
+          <div class="card-data widgetCardData">
+          <h2 class="box-title" style="color: #767A5B;">Excelente ubicación</h2></div>
+          </div>
+          </div>
+    </div>
+
+   
+      </div> -->
+    </div>
+  </div> <br><br>
+  <div class="section">
+    <div class="figure pull-left">
+    <img src="img/terrenos.jpg" class="img-responsive" alt="">
+    </div>
+
+    <div class="puntos pull-right">
+
+      <p class="textop"><b style="color: #81A41F;">Lotes</b> desde <b style="color: #81A41F;">390m2</b> hasta <b style="color: #81A41F;">587m2</b>  precio desde  <b style="color: #81A41F;">$160 el m2</b> . Escriturados al momento. <b style="color: #81A41F;">Precios</b> disponibles en <b style="color: #81A41F;">6, 12 y 24 meses</b> pregunta por nuestros <b style="color: #81A41F;">precios</b> por pago de <b style="color: #81A41F;">contado</b>.</p>
+
+      <div style="text-align: center; margin-top: 9%;">
+      <a class="btn btn-success btn-outline btn-lg" data-scroll href="#CONTACTO">¡COTIZA TU LOTE AHORA!</a>
+      </div>
+        <!-- <div class="row">
+          <div class="col-md-2">
+          </div>
+          <div class="col-md-6">
+            <h4 class="text-left">Lotes Desde 390 M2 hasta 587m2</h4>
+          </div>
+          </div> <br><br>
+          <div class="row">
+          <div class="col-md-2">
+          </div>
+          <div class="col-md-6">
+            <h4 class="text-left">Precio desde  $160 el m2 </h4>
+          </div>
+        </div><br><br>
+        <div class="row">
+          <div class="col-md-2">
+          </div>
+          <div class="col-md-6">
+            <h4 class="text-left">Escriturados al momento</h4>
+          </div>
+          </div><br><br>
+          <div class="row">
+          <div class="col-md-2">
+          </div>
+          <div class="col-md-6 text-center">
+            <h4 class="text-left">Precios disponibles en 6 12 y 24 meses </h4>
+          </div>
+        </div><br><br>
+        <div class="row">
+          <div class="col-md-2">
+          </div>
+          <div class="col-md-6 text-center">
+            <h4 class="text-left">Precios por pago de contado</h4>
+          </div>
+        </div> -->
+    </div>
+  </div>
+</section>
+
+<div class="banner2">
+  <div class="fondo2">
+    <p class="text-center tit2">Lotes Residenciales </p>
+    <p class="subtit2 text-center">¿Desea sentar su patrimonio en una zona de tranquilidad pero al mismo tiempo disfrutar de los mejores servicios y lugares de la ciudad?, entonces  CEIBAS ZIZILCHE  es una excelente opción.</p>
+    <div style="text-align: center; margin-top: 5%;">
+      <a class="btn btn-danger btn-outline btn-lg" data-scroll href="#CONTACTO">¡COTIZA TU LOTE AHORA!</a>
+  </div>
+  </div>
+</div>
+
+
+
+
+<section class="seccion3" id="CONTACTO">
+  <h1 class="text-center" style="color: #727A42;">CONTACTANOS</h1>
+
+  <div class="container">
+<div class="row">
+<div class="col-md-6">
+  <figure class="logo2">
+    <img src="img/ceibas.jpg" alt="CEIBAS" class="img-responsive">
+  </figure>
+<h3 style="margin-top: 0px;">Nuestros asesores le darán toda la información que necesita ¡CONTACTANOS AHORA!</h3>
+<hr>
+<address>
+<strong>Email:</strong> <a href="mailto:info@zizilche.com"> info@zizilche.com</a><br>
+<strong>Teléfono:</strong> (999) 40-61-108<br>
+<strong>Horario:</strong> Lun-Vie de 9 am a 6 pm<br>
+<strong>Dirección:</strong> Calle 7-A #279-E por 38 y 40 Fraccionamiento del Norte. <br>Mérida, Yucatán, México.  C.P. 97120
+</address>
+</div>
+    
+<div class="col-md-6 contact-form">
+<form id="contact" method="post" class="form" role="form">
+<div class="row">
+<div class=" col-md-12 form-group">
+  <!-- <label for="">Nombre</label> -->
+<input class="form-control" id="name" name="name" placeholder="Escribe tu nombre completo" type="text" required  />
+</div>
+<div class=" col-md-12 form-group">
+<input class="form-control" id="name" name="name" placeholder="Escribe tu número teléfonico" type="text" required  />
+</div>
+<div class=" col-md-12 form-group">
+<input class="form-control" id="email" name="email" placeholder="Escribe tu correo" type="email" required />
+</div>
+</div>
+<textarea class="form-control" id="message" name="message" placeholder="Escribe tu comentario" rows="5"></textarea>
+<br />
+<div class="row">
+<div class="col-xs-12 col-md-12 form-group">
+<button class="btn btn-success pull-right" type="submit">ENVIAR</button>
+</form>
+</div>
+</div>
+
+</section>
+
+
+
+<footer>
+<p>Derechos de autor 2017 © | CEIBAS </p>
+</footer>
+
+<span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="js/jquery.js"></script> -->
+    <script src="js/arriba.js"></script>
+    <!-- <script src="js/bootstrap.js"></script> -->
+    <script src="js/menu.js"></script>
+  </body>
+</html>
